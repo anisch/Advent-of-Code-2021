d28ce121b1679cf49c6bcaaafa0b81d22c015d1c
anisch <8927916+anisch@users.noreply.github.com>
2021-12-01 20:34:32 +0100

    Day01 - Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,5 @@
 fun main() {
-    fun countLargerMeasurements(input: List<String>): Int {
+    fun countLargerMeasurementsPart1(input: List<String>): Int {
         return input.map { string ->
             string.toInt()
         }.filterIndexed { index, i ->
@@ -8,11 +8,22 @@ fun main() {
         }.size
     }
 
+    fun countLargerMeasurementsPart2(input: List<String>): Int {
+        val tripleList = List(input.size - 2) { index ->
+            input[index].toInt() + input[index+1].toInt() + input[index+2].toInt()
+        }
+        return tripleList.filterIndexed { index, i ->
+            if (index == 0) false
+            else tripleList[index - 1] < i
+        }.size
+    }
+
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(countLargerMeasurements(testInput) == 7)
+    check(countLargerMeasurementsPart1(testInput) == 7)
+    check(countLargerMeasurementsPart2(testInput) == 5)
 
     val input = readInput("Day01")
-    println(countLargerMeasurements(testInput))
-    println(countLargerMeasurements(input))
+    println(countLargerMeasurementsPart1(input))
+    println(countLargerMeasurementsPart2(input))
 }
